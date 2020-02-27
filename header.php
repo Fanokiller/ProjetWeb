ed5fcad40a469e5eca00b20e62528cdfd522888c
Carrère <ludovic.carrere@viacesi.fr>
2020-02-27 16:29:34 +0100

    passage en reponsive 0.2

--- header.php
+++ header.php
@@ -85,4 +85,5 @@
 <div class="container-fluid">
     <!-- row -->
     <div class="row">
+    <div class="main-content flex-1 bg-gray-100 mt-12 md:mt-2 pb-24 md:pb-5">
         <!-- <?php include __DIR__ . '/../ProjetWeb/menu.php' ?> -->
