c6c287d77cfb2c491c5ce0a9c15adbf5bb99ac49
Carrère <ludovic.carrere@viacesi.fr>
2020-02-27 22:01:51 +0100

    Modif de localisation

--- erreur.php
+++ erreur.php
@@ -1,14 +1,13 @@
-<?php include __DIR__ . './header.php' ?>
-    <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
-        <div>
+<?php include __DIR__ . '/header.php' ?>
+<div class="main-content flex-1 bg-gray-100 mt-12 md:mt-2 pb-24 md:pb-5">        <div>
             <h1 class="border-bottom w-100">Formulaire</h1>
             <div class="alert alert-danger" role="alert">
                 Le formulaire envoyé est incorrect !
             </div>
             <div class="text-center lead">
-                <a href="./accueil/accueil.php">Retourner au formulaire</a>
+                <a href="./accueil/accueil.php">Retourner à l'accueil</a>
             </div>
         </div>
-    </main>
+</div>
 
-<?php include __DIR__ . './footer.php' ?>
+<?php include __DIR__ . '/footer.php' ?>
