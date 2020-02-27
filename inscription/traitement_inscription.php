7d012bafa713c4986dff149c273437894300a438
Carrère <ludovic.carrere@viacesi.fr>
2020-02-27 15:57:23 +0100

    ajout de fonction 2

--- inscription/traitement_inscription.php
+++ inscription/traitement_inscription.php
@@ -12,14 +12,15 @@ catch(Exception $e)
 }
 
 
-if (isset($_POST['Nom_inscription']) && isset($_POST['password'])) {
+if (isset($_POST['Nom_inscription']) && isset($_POST['password']) && isset($_POST['email'])) {
 
-    if (strlen($_POST['Nom_inscription']) != 0 && strlen($_POST['password']) != 0 ) {
+    if (strlen($_POST['Nom_inscription']) != 0 && strlen($_POST['password']) != 0 && strlen($_POST['email'])) {
 
-        $req = $bdd->prepare('INSERT INTO user (user_login,user_password) VALUES (:user_login,:user_password)');
+        $req = $bdd->prepare('INSERT INTO user (Nom_inscription,passeword,email) VALUES (:nickname,:password,:email)');
         $req->execute(array(
-            'user_login' => $_POST['Nom_inscription'],
-            'user_password' => $_POST['password']
+            'nickname' => $_POST['Nom_inscription'],
+            'password' => $_POST['password'],
+            'email' => $_POST['email']
             ));
             var_dump($req);
         header('Location: /../skeleton/formulaire/succes.php');
