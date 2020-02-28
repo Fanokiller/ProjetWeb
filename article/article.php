acb770c26b2f44e52c4271d279a7bf13090f3907
Carrère <ludovic.carrere@viacesi.fr>
2020-02-28 09:39:33 +0100

    Corps de l'article

---
+++ article/article.php
@@ -0,0 +1,12 @@
+<?php include __DIR__ . '/../header.php' ?>  <!-- Header -->
+<div class="main-content flex-1 bg-gray-100 mt-12 md:mt-2 pb-24 md:pb-5">
+    <div class="flex flex-wrap mt-12">
+        <div class="w-full md:w-1/2 xl:w-1/3 p-3 mx-auto">
+            <div class="bg-gray-900 border border-gray-800 rounded shadow p-2">
+                <div class="flex flex-row items-center">
+
+
+
+
+
+</div>
