f6c7881afc36d3ab6591ff9baf7f9b2df7dccbb7
Carrère <ludovic.carrere@viacesi.fr>
2020-02-28 16:10:21 +0100

    Debug Header 0.2

--- header.php
+++ header.php
@@ -56,22 +56,16 @@
 				    </li>
 				    <li class="flex-1 md:flex-none md:mr-3">
 						<div class="relative inline-block text-white">
-                            
+                            <button onclick="toggleDD('myDropdown')" class="drop-button text-white focus:outline-none">
                             <?php
                             if (isset($_SESSION['nickname'])) // Si le membre est connecté
                             {
                             echo 'Bonjour  '.$_SESSION['nickname'];
                             }
-                            ?>
-                            <button onclick="toggleDD('myDropdown')" class="drop-button text-white focus:outline-none">
-                            <i class="fas fa-caret-square-down fa-fw mr-1"></i></button>
-                            
-                            <div id="myDropdown" class="dropdownlist absolute bg-gray-900 text-white right-0 mt-3 p-3 overflow-auto z-30 invisible">
-                                <input type="text" class="drop-search p-2 text-gray-600" placeholder="Search.." id="myInput" onkeyup="filterDD('myDropdown','myInput')">
-                                <a href="#" class="p-2 hover:bg-gray-800 text-white text-sm no-underline hover:no-underline block"><i class="fa fa-user fa-fw"></i> Profile</a>
-                                <a href="#" class="p-2 hover:bg-gray-800 text-white text-sm no-underline hover:no-underline block"><i class="fa fa-cog fa-fw"></i> Settings</a>
-                                <div class="border border-gray-800"></div>
-                                <a href="#" class="p-2 hover:bg-gray-800 text-white text-sm no-underline hover:no-underline block"><i class="fas fa-sign-out-alt fa-fw"></i> Log Out</a>
+                            ?><svg class="h-3 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg></button>
+                            <div id="myDropdown" class="dropdownlist absolute bg-black text-white pin-r mt-3 p-3 overflow-auto z-30 invisible">
+                                <a href="#" class="hover:bg-grey-darkest text-white text-sm no-underline hover:no-underline block"><i class="fa fa-user fa-fw mr-1"></i>Profile</a>
+                                <a href="#" class="hover:bg-grey-darkest text-white text-sm no-underline hover:no-underline block"><i class="fa fa-cog fa-fw mr-1"></i>Settings</a>
                             </div>
                         </div>
                     </li>
