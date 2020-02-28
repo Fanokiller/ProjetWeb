2a9e2a9d62832f432900d66f86541bc6a537e29f
Jean-Baptiste Brausem <j.b.brausem@gmail.com>
2020-02-28 16:28:04 +0100

    refonte interface

--- accueil/accueil.php
+++ accueil/accueil.php
@@ -1,111 +1,31 @@
 <?php include __DIR__ . '/../header.php' ?>  <!-- Header -->
 <div class="main-content flex-1 bg-gray-100 mt-12 md:mt-2 pb-24 md:pb-5">
-    <div class="flex flex-wrap ">
-                <div class="w-full md:w-1/2 xl:w-1/3 p-3">
                     <!--Metric Card-->
-                    <div class="bg-gray-900 border border-gray-800 rounded shadow p-2">
-                        <div class="flex flex-row items-center">
-                            <div class="flex-shrink pr-4">
-                                <div class=>
-                                <img src=picture\sgec.jpg>
-                                </div>
-                            </div>
-                            <div class="flex-1 text-right md:text-center">
-                                <!-- <h5 class="font-bold uppercase text-gray-400">Total Revenue</h5>
-                                <h3 class="font-bold text-3xl text-gray-600">$3249 <span class="text-green-500"><i class="fas fa-caret-up"></i></span></h3> -->
-                            </div>
-                        </div>
-                    </div>
-                    <!--/Metric Card-->
+        <div class="bg-gray-900 border border-gray-800 rounded shadow p-1 mt-6 ml-6 mr-6">
+            <div class="md:flex flex-row items-center">
+                <div class=flex-auto>    
+                    
+                    <img src=picture\sgec.jpg class="xl:p-3 h-64 w-full">
                 </div>
-                <div class="w-full md:w-1/2 xl:w-1/3 p-3">
-                    <!--Metric Card-->
-                    <div class="bg-gray-900 border border-gray-800 rounded shadow p-2">
-                        <div class="flex flex-row items-center">
-                            <div class="flex-shrink pr-4">
-                                <div class=>
-                                <img src=picture\sgec.jpg>
-                                </div>
-                            </div>
-                            <div class="flex-1 text-right md:text-center">
-                                <!-- <h5 class="font-bold uppercase text-gray-400">Total Users</h5>
-                                <h3 class="font-bold text-3xl text-gray-600">249 <span class="text-orange-500"><i class="fas fa-exchange-alt"></i></span></h3> -->
-                            </div>
-                        </div>
-                    </div>
-                    <!--/Metric Card-->
-                </div>
-                <div class="w-full md:w-1/2 xl:w-1/3 p-3">
-                    <!--Metric Card-->
-                    <div class="bg-gray-900 border border-gray-800 rounded shadow p-2">
-                        <div class="flex flex-row items-center">
-                            <div class="flex-shrink pr-4">
-                                <div class=>
-                                <img src=picture\sgec.jpg>
-                                </div>
-                            </div>
-                            <div class="flex-1 text-right md:text-center">
-                                <!-- <h5 class="font-bold uppercase text-gray-400">New Users</h5>
-                                <h3 class="font-bold text-3xl text-gray-600">2 <span class="text-yellow-600"><i class="fas fa-caret-up"></i></span></h3> -->
-                            </div>
-                        </div>
-                    </div>
-                    <!--/Metric Card-->
-                </div>
-                <div class="w-full md:w-1/2 xl:w-1/3 p-3">
-                    <!--Metric Card-->
-                    <div class="bg-gray-900 border border-gray-800 rounded shadow p-2">
-                        <div class="flex flex-row items-center">
-                            <div class="flex-shrink pr-4">
-                                <div class=>
-                                <img src=picture\sgec.jpg>
-                                </div>
-                            </div>
-                            <div class="flex-1 text-right md:text-center">
-                                <!-- <h5 class="font-bold uppercase text-gray-400">Server Uptime</h5>
-                                <h3 class="font-bold text-3xl text-gray-600">152 days</h3> -->
-                            </div>
-                        </div>
-                    </div>
-                    <!--/Metric Card-->
-                </div>
-                <div class="w-full md:w-1/2 xl:w-1/3 p-3">
-                    <!--Metric Card-->
-                    <div class="bg-gray-900 border border-gray-800 rounded shadow p-2">
-                        <div class="flex flex-row items-center">
-                            <div class="flex-shrink pr-4">
-                                <div class=>
-                                <img src=picture\sgec.jpg>
-                                </div>
-                            </div>
-                            <div class="flex-1 text-right md:text-center">
-                                <!-- <h5 class="font-bold uppercase text-gray-400">To Do List</h5>
-                                <h3 class="font-bold text-3xl text-gray-600">7 tasks</h3> -->
-                            </div>
-                        </div>
-                    </div>
-                    <!--/Metric Card-->
-                </div>
-                <div class="w-full md:w-1/2 xl:w-1/3 p-3">
-                    <!--Metric Card-->
-                    <div class="bg-gray-900 border border-gray-800 rounded shadow p-2">
-                        <div class="flex flex-row items-center">
-                            <div class="flex-shrink pr-4">
-                                <div class=>
-                                <img src=picture\sgec.jpg>
-                                </div>
-                            </div>
-                            <div class="flex-1 text-right md:text-center">
-                                <!-- <h5 class="font-bold uppercase text-gray-400">Issues</h5>
-                                <h3 class="font-bold text-3xl text-gray-600">3 <span class="text-red-500"><i class="fas fa-caret-up"></i></span></h3> -->
-                            </div>
-                        </div>
-                    </div>
-                    <!--/Metric Card-->
+                <div class="flex md: pr-6">
+                     <p class="text-right font-bold uppercase text-gray-400">Total Revenue Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores, fugiat veritatis perspiciatis dignissimos esse aspernatur quia suscipit voluptatibus natus explicabo eius aut molestiae repellat, sunt cupiditate a accusantium dicta sequi! </p> 
                 </div>
             </div>
+        </div>
+        <div class="bg-gray-900 border border-gray-800 rounded shadow p-1 mt-6 ml-6 mr-6">
+            <div class="md:flex flex-row items-center">
+            <div class="flex md: pr-6">
+                     <p class="text-left font-bold uppercase text-gray-400">Total Revenue Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores, fugiat veritatis perspiciatis dignissimos esse aspernatur quia suscipit voluptatibus natus explicabo eius aut molestiae repellat, sunt cupiditate a accusantium dicta sequi! </p> 
+                </div>
 
 
+                <div class=flex-auto>    
+                    
+                    <img src=picture\sgec.jpg class="xl:p-3 h-64 w-full">
+                </div>
+                
+            </div>
+        </div>  
 </div>
 <?php include __DIR__ . '/../footer.php' ?> <!-- Footer -->
 
@@ -113,3 +33,4 @@
 
 
 <!-- <div class="rounded p-3 bg-green-600"><i class="fa fa-wallet fa-2x fa-fw fa-inverse"></i></div> -->
+
