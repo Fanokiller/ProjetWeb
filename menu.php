ad66907eb96004462427faf03e0d4e9c46e99dcc
Carrère <ludovic.carrere@viacesi.fr>
2020-02-28 10:38:58 +0100

    Ajout du caddie connecté ou déconnecté

--- menu.php
+++ menu.php
@@ -13,14 +13,25 @@
             </li>
             <li class="mr-3">
                 <a href="../saison/saison.php" class="block py-1 md:py-3 pl-1 align-middle text-white no-underline hover:text-white border-b-2 border-gray-800 hover:border-purple-500">
-                    <i class="fas fa-search fa-fw mr-1"></i><span class="pb-1 md:pb-0 text-xs md:text-base text-gray-600 md:text-gray-400 block md:inline-block ml-2">Aperçu</span>
+                    <i class="fas fa-search fa-fw mr-1"></i><span class="pb-1 md:pb-0 text-xs md:text-base text-gray-600 md:text-gray-400 block md:inline-block ml-2">Saison</span>
                 </a>
             </li>
+            <?php
+             if (isset($_SESSION['nickname'])):
+
+                ?>
             <li class="mr-3">
-                <a href="/../siteJeux/résumé/résumé.php" class="block py-1 md:py-3 pl-1 align-middle text-white no-underline hover:text-white border-b-2 border-gray-800 hover:border-green-300">
-                    <i class="far fa-bookmark fa-fw mr-1"></i><span class="pb-1 md:pb-0 text-xs md:text-base text-gray-600 md:text-gray-400 block md:inline-block ml-2">Résumé</span>
+                <a href="/../ProjetWeb/cart/cart.php" class="block py-1 md:py-3 pl-1 align-middle text-white no-underline hover:text-white border-b-2 border-gray-800 hover:border-green-300">
+                <i class="fas fa-shopping-cart fa-fw mr-1"></i><span class="pb-1 md:pb-0 text-xs md:text-base text-gray-600 md:text-gray-400 block md:inline-block ml-2">Cart</span>
                 </a>
             </li>
+            <?php else :  ?>
+                <li class="mr-3">
+                <a href="/../ProjetWeb/cart/cart.php" class="block py-1 px-3 md:py-3 pl-1 text-gray-400 align-middle text-white cursor-not-allowed">
+                <i class="fas fa-shopping-cart fa-fw mr-1"></i><span class="pb-1 md:pb-0 text-xs md:text-base text-gray-600 md:text-gray-400 block md:inline-block ml-2">Cart</span>
+                </a>
+            </li>
+            <?php endif ?>
             <li class="mr-3">
                 <a href="/../siteJeux/formulaire/formulaire.php" class="block py-1 md:py-3 pl-0 md:pl-1 align-middle text-white no-underline hover:text-white border-b-2 border-gray-800 hover:border-red-500">
                     <i class="far fa-plus-square fa-fw mr-1"></i><span class="pb-1 md:pb-0 text-xs md:text-base text-gray-600 md:text-gray-400 block md:inline-block ml-2">Ajout d'un recette</span>
