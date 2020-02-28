055008b00cab9d4468e9df70e3b1b151563b657e
Carrère <ludovic.carrere@viacesi.fr>
2020-02-28 14:43:11 +0100

    Debug succes 0.1

--- succes.php
+++ succes.php
@@ -1,4 +1,4 @@
-<?php include __DIR__ . './header.php' ?>
+<?php include __DIR__ . '/../ProjetWeb/header.php' ?>
 
         <div class="main-content flex-1 bg-gray-400 mt-12 md:mt-2 pb-24 md:pb-5">
             <h1 class="border-bottom w-100">Formulaire</h1>
@@ -11,4 +11,4 @@
         </div>
 
 
-<?php include __DIR__ . './footer.php' ?>
+<?php include __DIR__ . '/../ProjetWeb/footer.php' ?>
