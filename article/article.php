2fd43a79b3caf69ce575b8024e8c4fd54141680c
Hülya <hulya.isik@viacesi.fr>
2020-03-02 16:17:41 +0100

    Présentation recette

--- article/article.php
+++ article/article.php
@@ -1,12 +1,57 @@
-<?php include __DIR__ . '/../header.php' ?>  <!-- Header -->
-<div class="main-content flex-1 bg-gray-100 mt-12 md:mt-2 pb-24 md:pb-5">
-    <div class="flex flex-wrap mt-12">
-        <div class="w-full md:w-1/2 xl:w-1/3 p-3 mx-auto">
-            <div class="bg-gray-900 border border-gray-800 rounded shadow p-2">
-                <div class="flex flex-row items-center">
+<?php include __DIR__ . '/../header.php' ?>
+    <!-- Header -->
+    <div class="main-content flex-1 bg-gray-100 mt-12 md:mt-2 pb-24 md:pb-5">
+        <div class="flex flex-wrap mt-12">
+            <div class="w-full md:w-1/2 p-3 mx-auto">
+                <div class="bg-red-200 border border-gray-800 rounded shadow p-2 text-center">
+                    <div class="flex flex-row">
+                        <span class="text-center"><h1> Saucisse apéro </h1>
+                        <img src="https://cac.img.pmdstatic.net/fit/http.3A.2F.2Fprd2-bone-image.2Es3-website-eu-west-1.2Eamazonaws.2Ecom.2Fcac.2F2018.2F09.2F25.2F3c877540-564b-4959-9880-9bbbbdb72e65.2Ejpeg/748x372/quality/80/crop-from/center/saucisse-feuillete-apero.jpeg"></span>
+                    </div>
+                    <div class="w-full p-3 border border-grey-300 rounded shadow p-2">
+                        <div class="mx-auto self-start pb-5"><i class="far fa-heart fa-5x"></i></div>
+                        <div class="flex justify-between">
+                            <button><span class="mx-auto"><i class="far fa-file-pdf fa-2x"></i></span></button>
+                            <button><span class="mx-auto"><i class="far fa-comment-dots fa-2x"></i></span></button>
+                            <button><span class="mx-auto"><i class="far fa-share-square fa-2x"></i></span></button>
+                            <button><span class="mx-auto"><i class="fas fa-print fa-2x"></i></span></button>
+                            <button><span class="mx-auto"><i class="far fa-star fa-2x"></i></span></button>
+                        </div>
+                        <div class="flex flew-row bg-white mt-4 rounded-lg">
+                            <div class="flex-shrink-0 mb-8 bg-gray-100 rounded-lg">
+                                <div class="flex text-center text-gray-600 font-bold text-xl mb-2 mr-4"><img class="w-10 h-10 rounded-full mr-4 inline-block" src="https://specials-images.forbesimg.com/imageserve/1005962360/960x0.jpg?fit=scale">Ingrédient</div>
+                                    <div class="flex items-center">
+                                        <img class="w-10 h-10 rounded-full mr-4 bg-gray-200" src="https://images.anaca3.com/wp-content/uploads/2018/07/les-knackis-autorise-pendant-la-grossesse-702x336.jpg">
+                                    </div>
+                                
+                            </div>
 
+                            <div class="mb-6">
+                                <div class="recipe-InstructionContent text-gray-900 text-center font-bold text-xl mb-2 block"><img class="w-10 h-10 rounded-full mr-4 inline-block" src="https://img2.freepng.fr/20180705/xzf/kisspng-whisk-drawing-kitchen-utensil-tool-wisk-5b3de1a20366c5.4073021715307821140139.jpg">Etape de Préparation</div>
+                                <ol class="list-decimal list-outside pl-8">
+                                    <li class="text-gray-700 text-base text-left ">Préchauffez votre four à 180°C</li><br>
 
+                                    <li class="text-gray-700 text-base text-left ">Déroulez la pâte feuilletée sur le plan de travail. Coupez des bandes de 5 cm de long et de 2 cm de large.</li><br>
 
+                                    <li class="text-gray-700 text-base text-left">Déposez un morceau de saucisse à l'extrémité de chaque morceau de pâte. Parsemez de gruyère râpé et roulez le tout.</li><br>
 
+                                    <li class="text-gray-700 text-base text-left">Tartinez les bandes d'un peu de moutarde.</li><br>
 
-</div>
+                                    <li class="text-gray-700 text-base text-left">Déposez sur une plaque allant au four. Mouillez légèrement au doigt le dessus de chaque feuilleté et parsemez de grains de sésame.</li><br>
+                                </ol>
+                            </div>
+                        </div>
+                        
+                            
+                        </div>
+                    </div>
+                    
+                        
+                    </div>
+                </div>
+            </div>
+            <div>
+            </div>
+        </div>
+
+    </div>
