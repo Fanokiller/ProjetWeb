3a20c3d81a381b5ab8a671827d6fb49fbf7ce31b
Jean-Baptiste Brausem <j.b.brausem@gmail.com>
2020-03-04 10:31:05 +0100

    responsive + caroussel op dans accueil

--- accueil/accueil.php
+++ accueil/accueil.php
@@ -1,9 +1,10 @@
 <?php include __DIR__ . '/../header.php' ?>  
 
-<div class="main-content flex-1 bg-gray-100 mt-12 md:mt-2 pb-24 md:pb-5">
+<div class="main-content md:flex-1 bg-gray-100 mt-12 md:mt-2 pb-24 md:pb-5">
 
     <!-- Partie Css pour que le caroussel soit fonctionnel  -->
     <link rel="stylesheet" href="carousselcss.css" />
+    <link rel="stylesheet" href="tablecss.css" />
 
     <style>
     .snap-x {
@@ -25,57 +26,61 @@
     <!-- caroussel 1ère image -->
 
 
-
-
-<div class="container">
-    <div class="carousel pt-6">
-        <div class="carousel-prev"></div>
-        <div class="carousel-next"></div>
-        <ul class="carousel-pagination">
-            <li class="carousel-bullet"></li>
-            <li class="carousel-bullet"></li>
-            <li class="carousel-bullet"></li>
-            <li class="carousel-bullet"></li>
-            <li class="carousel-bullet"></li>
-        </ul>
-        <ul class="carousel-container">
-            <li class="carousel-item">
-                <div>
-                <img src="picture\carentr.jpeg" class="w-full items-center h-full">
-                </div>
-            </li>
-            <li class="carousel-item">
-            <div>
-                <img src="picture\carent.jpg" class="w-full">
-                </div>
-            </li>
-            <li class="carousel-item">
-            <div>
-                <img src="picture\carplat.jpg" class="w-full">
-                </div>
-            </li>
-            <li class="carousel-item">
-            <div>
-                <img src="picture\cardes.jpg" class="w-full">
-                </div>
-            </li>
-            <li class="carousel-item">
-            <div>
-                <img src="picture\carap.jpg" class="w-full">
-                </div>
-            </li>
-        </ul>
+    <div class="md:pt-6">
     </div>
-</div>
+
+    <div class="container">
+        <div class="carousel">
+            <div class="carousel-prev">
+            </div>
+            <div class="carousel-next">
+            </div>
+            <ul class="carousel-pagination">
+                <li class="carousel-bullet"></li>
+                <li class="carousel-bullet"></li>
+                <li class="carousel-bullet"></li>
+                <li class="carousel-bullet"></li>
+                <li class="carousel-bullet"></li>
+            </ul>
+            <ul class="carousel-container">
+                <li class="carousel-item ">
+                    <div class="h-1/2">
+                        <img src="picture\carentr.jpeg" class="w-full items-center ">
+                    </div>
+                </li>
+                <li class="carousel-item">
+                    <div>
+                        <img src="picture\carent.jpg" class="w-full ">
+                    </div>
+                </li>
+                <li class="carousel-item ">
+                    <div class="h-8">
+                        <img src="picture\carplat.jpg" class="w-full ">
+                    </div>
+                </li>
+                <li class="carousel-item">
+                    <div>
+                        <img src="picture\cardes.jpg" class="w-full ">
+                    </div>
+                </li>
+                <li class="carousel-item">
+                    <div>
+                        <img src="picture\carap.jpg" class="w-full ">
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
 
 
                 <!-- fin caroussel -->
 
-                <!-- Début tableau  -->
+                <!-- Début tableau format < 640px -->
                 
-                <!-- Premier tableau (image gauche texte droite) -->
-        <table class="w-2 md:w-full h-2 bg-white text-right border-4 border-gray-800  ">       
-            <thead>
+               <!-- Premier tableau (image gauche texte droite) -->
+            <div id="testr">
+               <table class="w-2 w-full h-2 bg-white text-right border-4 border-gray-800  ">       
+                <thead>
                 <tr>
                     <th class="px-4 py-2 bg-gray-800 text-blue-200 w-1/3"></th>
                     <th class="px-4 py-2 bg-gray-800 text-blue-200 w-2/3"></th>
@@ -84,10 +89,10 @@
 
             <tbody>
                 <tr class="border-4 border-gray-800">
-                    <td class="flex-none px-4 py-2 w-1/3" > 
-                        <img class=" inline w-full mr-4  flex-none" src="picture\sgec.jpg ">
+                    <td class="md:flex-none px-4 py-2 w-1/3" > 
+                        <img class=" inline w-full mr-4  md:flex-none" src="picture\sgec.jpg ">
                     </td>
-                    <td class=" px-4 py-2 flex-none text-gray-800 w-2/3">
+                    <td class=" px-4 py-2 md:flex-none text-gray-800 w-2/3">
                         <p>teconsectetur aeos itaque, soluta voluptate quod illo ipsam.lorem Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe cupiditate maxime, eaque tenetur inventore quo fugit illo assumenda, amet adipisci laborum corporis, architecto nostrum minima reiciendis porro! Accusantium, id quae? lore </p>
                     </td>
                 </tr>
@@ -108,7 +113,7 @@
             </thead>
             <tbody>
                 <tr>
-                    <td class="flex-none px-4 py-2 " > 
+                    <td class="md:flex-none px-4 py-2 " > 
                         <p>teconsectetur aeos itaque, soluta voluptate quod illo ipsam.lorem Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe cupiditate maxime, eaque tenetur inventore quo fugit illo assumenda, amet adipisci laborum corporis, architecto nostrum minima reiciendis porro! Accusantium, id quae? lore </p>
                     </td>
                     <td class=" px-4 py-2 flex-none">
@@ -117,8 +122,43 @@
                 </tr>    
             </tbody>
         </table>
+</div>
+
+
+
+<div id="testi">
+                <table class="bg-white  border-4 border-gray-800  ">       
+                    <tbody>
+                        <tr>
+                            <img class="border-4 border-gray-800" src="picture\sgec.jpg ">
+                            <td class=" flex-none text-center text-gray-800">
+                                <p>teconsectetur aeos itaque, soluta voluptate quod illo ipsam.lorem Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe cupiditate maxime, eaque tenetur inventore quo fugit illo assumenda, amet adipisci laborum corporis, architecto nostrum minima reiciendis porro! Accusantium, id quae? lore </p>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                 
+
+
+                <!-- 2nd tableau (image droite texte gauche) -->
+
+                <table class="bg-white  border-4 border-gray-800  ">       
+                    <tbody>
+                        <tr>
+                            <img class="border-4 border-gray-800" src="picture\sgec.jpg ">
+                            <td class=" flex-none text-center text-gray-800">
+                                <p>teconsectetur aeos itaque, soluta voluptate quod illo ipsam.lorem Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe cupiditate maxime, eaque tenetur inventore quo fugit illo assumenda, amet adipisci laborum corporis, architecto nostrum minima reiciendis porro! Accusantium, id quae? lore </p>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table> 
+</div>
+
         <script type="text/javascript" src="carousseljs.js"></script>
 </div>
+<style>
+
+
 
 <?php include __DIR__ . '/../footer.php' ?>
 
