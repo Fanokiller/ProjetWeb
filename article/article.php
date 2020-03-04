93f074ba037b8df2eab5616725116d1a63702609
Hülya <hulya.isik@viacesi.fr>
2020-03-04 16:53:07 +0100

    Article 1.1

--- article/article.php
+++ article/article.php
@@ -1,25 +1,24 @@
 <?php include __DIR__ . '/../header.php' ?>
     <!-- Header -->
-    <div class="main-content flex-1 bg-gray-100 mt-12 md:mt-2 pb-24 md:pb-5">
-        <div class="flex flex-wrap mt-12">
+    <div class="main-content flex-1 bg-white mt-12 md:mt-2 pb-24 md:pb-5">
+        <div class="flex flex-wrap mt-6">
             <div class="w-full md:w-1/2 p-3 mx-auto">
-                <div class="bg-red-100 border border-gray-800 rounded shadow p-2 text-center">
-                    <div class="container pb-4 pt-2 bg-light border rounded border-light">
+                <div class="bg-red-100 rounded shadow p-2 text-center">
+                    <div class="container bg-light border rounded border-light">
                         <span class="text-center font-bold text-5xl mx-auto">
-                            <div class="col-sm">
-                                <h1 class=" bg-red-300 border rounded border-b-2 border-gray-500"> Saucisse apéro </h1></span>             
-                            </div>     
-                    <div class="flex inline-block text-left">
-                        <span class="inline-block text-center text-lg"><i class="text-black fas fa-stopwatch" style="color:black"></i><br>
+                            <p class=" bg-red-500"style="color:white"> Saucisse apéro </p><br>
+                        </span>              
+                    <div class="flex justify-between pb-4">
+                        <span class="inline-block mx-auto text-lg "><i class="fas fa-stopwatch h-10 w-10 fa-2x" style="color:gray"></i><br>
                         20 minutes</span>
-                        <span class="inline-block text-center"> 
-                        <i class="fas fa-circle h-10 w-10" style="color:gray"></i></span>
-                        <span class="inline-block text-center"><i class="fas fa-circle h-10 w-10" style="color:gray"></i></span>
-                        <span class="inline-block text-center"><i class="fas fa-circle h-10 w-10" style="color:black"></i>Facilité</span>
-                        <span class="inline-block text-center text-lg"><i class="fas fa-circle h-10 w-10" style="color:black"></i>Facilité</span>
-                        <span class="inline-block text-left"><i class="fas fa-euro-sign"style="color:gray"></i></span>
-                        <span class="inline-block text-left"><i class="fas fa-user-alt" style="color:gray"></i></span>
-                        <br>
+                        <span class="inline-block mx-auto text-lg"> 
+                        <i class="fas fa-circle h-10 w-10 fa-2x" style="color:green"></i>
+                        <i class="fas fa-circle h-10 w-10 fa-2x" style="color:gray"></i>
+                        <i class="fas fa-circle h-10 w-10 fa-2x" style="color:gray"></i><br>Facile</span>                        
+                        <span class="inline-block mx-auto text-lg "><i class="fas fa-euro-sign mr-1 h-10 w-10 fa-2x"style="color:gray"></i><br>
+                        Bon marché</span>
+                        <span class="inline-block mx-auto text-lg">
+                        <i class="fas fa-user-alt h-10 w-10 fa-2x" style="color:gray"></i><br> 4</span>
                     </div>
                     <img class="mx-auto" src="https://cac.img.pmdstatic.net/fit/http.3A.2F.2Fprd2-bone-image.2Es3-website-eu-west-1.2Eamazonaws.2Ecom.2Fcac.2F2018.2F09.2F25.2F3c877540-564b-4959-9880-9bbbbdb72e65.2Ejpeg/748x372/quality/80/crop-from/center/saucisse-feuillete-apero.jpeg"></span>
                 </div>
@@ -33,19 +32,8 @@
                     </div>
                     <div class="border-2 border-gray-400 mt-4"></div>
                     <div class="flex flew-row bg-white rounded-lg">
-                        <div class="flex-shrink-0 mb-8 bg-gray-600 rounded-lg">
+                        <div class="flex-shrink-0 bg-gray-600 rounded-lg h-full">
                             <div class="flex text-center text-white font-bold text-xl mb-2 block mr-4 mt-4"><img class="w-10 h-10 rounded-full mr-4 inline-block" src="https://specials-images.forbesimg.com/imageserve/1005962360/960x0.jpg?fit=scale">Ingrédient</div>
-                            <!-- <tbody>
-                            <tr class="border-4 border-gray-800">
-                                <td class="flex-none px-4 py-2 w-1/3" > 
-                                <p class="m-2"><img class="w-10 h-10 rounded-full mr-4 bg-gray-200 inline-block" src="https://images.anaca3.com/wp-content/uploads/2018/07/les-knackis-autorise-pendant-la-grossesse-702x336.jpg">1 paquet de knackis</p>
-                                </td>
-                                <td class="flex-none px-4 py-2 w-1/3" > 
-                                <p class="m-2 -ml-3"><img class="w-10 h-10 rounded-full mr-4 bg-gray-200 inline-block" src="https://www.herta.fr/sites/default/files/images/product/principal/864b443e-88de-4573-bcb7-87e5b86d2e9d.png">1 pâte feuilletée</p>
-                                </td>
-                            </tr>
-                        </tbody> -->
-
                             <ul>
                                 <li class="flex px-4 py-2">
                                     <p class="m-2 -ml-3 text-white"><img class="w-10 h-10 rounded-full mr-4 bg-gray-200 inline-block" src="https://images.anaca3.com/wp-content/uploads/2018/07/les-knackis-autorise-pendant-la-grossesse-702x336.jpg">1 paquet de knackis</p>
@@ -66,24 +54,6 @@
                             </ul>
 
                         </div>
-                        <!-- </table>        
-    <table class="w-2 md:w-full h-2 bg-white text-left border-4 border-gray-800 ">
-        <thead>
-            <tr>
-                <th class="px-4 py-2 bg-gray-800 text-blue-200 w-2/3 "></th>
-                <th class="px-4 py-2 bg-gray-800 text-blue-200 w-1/3"></th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td class="flex-none px-4 py-2 " > 
-                    <p>teconsectetur aeos itaque, soluta voluptate quod illo ipsam.lorem Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe cupiditate maxime, eaque tenetur inventore quo fugit illo assumenda, amet adipisci laborum corporis, architecto nostrum minima reiciendis porro! Accusantium, id quae? lore </p>
-                </td>
-                <td class=" px-4 py-2 flex-none">
-                    <img class=" inline w-full mr-4  flex-none" src="picture\sgec.jpg ">
-                </td>
-            </tr>     -->
-
                         <div class="mb-6 flex-shrink-1">
                             <div class="text-gray-900 text-center font-bold text-xl mb-2 block mt-4"><img class="w-10 h-10 rounded-full mr-4 inline-block" src="https://img2.freepng.fr/20180705/xzf/kisspng-whisk-drawing-kitchen-utensil-tool-wisk-5b3de1a20366c5.4073021715307821140139.jpg">Etape de Préparation</div>
                             <ol class="list-decimal list-outside pl-8">
