e3a3156fe587de4a434a94ce1a842be7b02e441c
Willem <willem.daoud@viacesi.fr>
2020-03-05 11:46:37 +0100

    Recette

--- menu.php
+++ menu.php
@@ -8,7 +8,7 @@
             </li>
             <li class="mr-3">
                 <a href="../recette/recette.php" class="block py-1 md:py-3 pl-1 align-middle text-white no-underline hover:text-white border-b-2 border-gray-800 hover:border-pink-500">
-                    <i class="fas fa-gamepad fa-fw mr-1"></i><span class="pb-1 md:pb-0 text-xs md:text-base text-gray-600 md:text-gray-400 block md:inline-block ml-2">Recettes</span>
+                    <i class="fas fa-gamepad fa-fw mr-1"></i><span class="pb-1 md:pb-0 text-xs md:text-base text-gray-600 md:text-gray-400 block md:inline-block ml-2">Recette</span>
                 </a>
             </li>
             <li class="mr-3">
