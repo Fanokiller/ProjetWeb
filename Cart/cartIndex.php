8aa013b35115c182d04cb2c83f7c9cd6bfb3cf88
Carrère <ludovic.carrere@viacesi.fr>
2020-03-05 10:27:40 +0100

    Debug visuel 0.1

--- Cart/cartIndex.php
+++ Cart/cartIndex.php
@@ -1,4 +1,6 @@
 <?php include __DIR__ . '/../header.php' ?>
+<div class="main-content md:flex-1 bg-gray-100 mt-12 md:mt-2 pb-24 md:pb-5">
+
 <?php
     var_dump($DB->query('SELECT * FROM test'));
     if(isset($_GET['id'])){
@@ -7,4 +9,5 @@
         die('Vous n\'avez pas séléctionné de produit à ajouter dans votre panier');
     }
 ?>
+</div>
 <?php include __DIR__ . '/../footer.php' ?>
