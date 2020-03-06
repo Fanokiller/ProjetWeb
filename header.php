743d783e24f5eacc8a0f91a12f4b8397aefa000a
Carrère <ludovic.carrere@viacesi.fr>
2020-03-06 12:32:01 +0100

    Debug 0.6

--- header.php
+++ header.php
@@ -1,8 +1,8 @@
 <?php
 session_start(); 
-require 'Fonction/db.class.php';
+require 'fonction/db.class.php';
 $DB = new DB();
-require 'Fonction/cart.class.php';
+require 'fonction/cart.class.php';
 $panier = new panier();
 
 ?>
@@ -20,12 +20,8 @@ $panier = new panier();
     <!-- Bootstrap & Tailwind core CSS -->
     <link href="https://unpkg.com/tailwindcss@%5E1.0/dist/tailwind.min.css" rel="stylesheet">
     <!-- Custom styles for this template -->
-
-    <link href="/../skeleton/asset/dashboard.css" rel="stylesheet">
-
     <link href="/../ProjetWeb/article/Article.css" rel="stylesheet">
     <link href="/../ProjetWeb/asset/css/style.css" rel="stylesheet">
-
     <link href="/../ProjetWeb/asset/css/carousselcss.css" rel="stylesheet">
     <link href="/../ProjetWeb/asset/css/tablecss.css" rel="stylesheet">
 
@@ -34,17 +30,18 @@ $panier = new panier();
 <body class="bg-white font-sans leading-normal tracking-normal mt-12">
     <nav class="bg-teal-500 pt-1 md:pt-2 pb-1 px-1 mt-0 h-auto fixed w-full z-20 top-0 px-4">
         <div class="flex flex-shrink justify-center md:justify-start text-white">
-        <ul class="list-reset flex justify-between flex-1 md:flex-none items-center">
-            <li class="mr-3">
+            <ul class="list-reset flex justify-between flex-1 md:flex-none items-center">
+                <li class="mr-3">
                     <a href="../accueil/accueil.php" class="inline-block py-1 md:py-3 pl-1 align-middle text-white no-underline hover:text-white border-b-2 border-teal-500 hover:border-white">
                         <i class="fas fa-home fa-fw mr-1"></i><span class="pb-1 md:pb-0 text-xs md:text-base text-white md:text-white inline-block md:inline-block ml-2">Accueil</span>
                     </a>
                 </li>
                 <li class="mr-3">
                     <a href="../index/index.php" class="block py-1 md:py-3 pl-1 align-middle text-white no-underline hover:text-white border-b-2 border-teal-500 hover:border-pink-500">
-                        <i class="fas fa-gamepad fa-fw mr-1"></i><span class="pb-1 md:pb-0 text-xs md:text-base text-white md:text-white block md:inline-block ml-2">Recettes</span>
+                        <i class="fas fa-bars fa-fw mr-1"></i><span class="pb-1 md:pb-0 text-xs md:text-base text-white md:text-white block md:inline-block ml-2">Recettes</span>
                     </a>
-                <li class="mr-3 dropdown">
+                </li>
+                    <li class="mr-3 dropdown">
                         <button class="dropbtnSaison" id="btnSaison">
                             <i class="fas fa-search fa-fw mr-1"></i>Saison 
                             <i class="fa fa-caret-down fa-fw mr-1 pt-1 pl-3"></i>
@@ -55,8 +52,8 @@ $panier = new panier();
                             <a href="#"><i class="fas fa-leaf fa-fw mr-1"></i>Automne</a>
                             <a href="#"><i class="fas fa-snowflake fa-fw mr-1"></i>Hiver</a>
                         </div>
-                </li>
-                </li>
+                    </li>
+                
                 <li class="mr-3">
                     <?php
                 if (isset($_SESSION['nickname'])):
@@ -91,7 +88,7 @@ $panier = new panier();
             </ul>
             
 
-            <div class="flex flex-1 md:w-1/3 justify-center md:justify-center text-black px-4 items-center">
+            <!-- <div class="flex flex-1 md:w-1/3 justify-center md:justify-center text-black px-4 items-center">
                 <span class="relative w-full">
                     <input type="search" placeholder="Search" class="w-full bg-white text-sm text-black transition border border-transparent focus:outline-none focus:border-gray-700 rounded py-1 px-2 pl-10 appearance-none leading-normal">
                     <div class="absolute search-icon" style="top: .5rem; left: .8rem;">
@@ -100,11 +97,11 @@ $panier = new panier();
                         </svg>
                     </div>
                 </span>
-            </div>
-				<ul class="list-reset flex justify-between flex-1 md:flex-none items-center px-4 mx-auto">
-                    <li class="flex-1 md:flex-none md:mr-3">
+            </div> -->
+				<ul class="list-reset flex justify-between flex-1 md:flex-none items-center mx-auto px-4">
+                    <!-- <li class="flex-1 md:flex-none md:mr-3">
 					    <a class="inline-block text-white no-underline hover:text-gray-200 hover:text-underline py-2 px-4" href="/../ProjetWeb/inscription/inscription.php"><i class="fas fa-user-plus fa-fw mr-1"></i> Inscription</a>
-				    </li>
+				    </li> -->
                         <?php
                             if (isset($_SESSION['nickname'])):
 
@@ -114,12 +111,18 @@ $panier = new panier();
                                     Deconnexion
                                 </a>
                             </li>
+                            <li class="flex-1 md:flex-none md:mr-3">
+					            <a class="inline-block text-white no-underline hover:text-gray-200 hover:text-underline py-2 px-4 hidden" href="/../ProjetWeb/inscription/inscription.php"><i class="fas fa-user-plus fa-fw mr-1"></i> Inscription</a>
+				            </li>
                         <?php else :  ?>
                             <li class="flex-1 md:flex-none md:mr-3">
                                 <a class="inline-block text-white no-underline hover:text-gray-200 hover:text-underline py-2 px-4" href="/../ProjetWeb/connexion/connexion.php">
                                     Connexion
                                 </a>
                             </li>
+                            <li class="flex-1 md:flex-none md:mr-3">
+					            <a class="inline-block text-white no-underline hover:text-gray-200 hover:text-underline py-2 px-4" href="/../ProjetWeb/inscription/inscription.php"><i class="fas fa-user-plus fa-fw mr-1"></i> Inscription</a>
+				            </li>
                         <?php endif ?>
 
 				    <li class="flex-1 md:flex-none md:mr-3">
@@ -128,13 +131,15 @@ $panier = new panier();
                                 <?php
                                     if (isset($_SESSION['nickname'])) // Si le membre est connecté
                                     {
-                                        echo 'Bonjour  '.$_SESSION['nickname'];
+                                        echo 'Bonjour  '.$_SESSION['nickname'];                                    
+                                        $id_user = $_SESSION['nickname'];
                                     }
+
                                 ?>
                                 <i class="fa fa-caret-down fa-fw mr-1 pt-1 pl-3"></i>
                             </button>
                             <div id="DropdownUser" class="dropdown-content text-white ml-4 show" >
-                                <a href="/../ProjetWeb/user/user.php"><i class="fas fa-user fa-fw mr-1"></i>User</a>
+                                <a href="/../ProjetWeb/user/user.php?id_user=<?= $id_user?>"><i class="fas fa-user fa-fw mr-1"></i>User</a>
                                 <a href="/../ProjetWeb/admin/adminIndex.php"><i class="fas fa-user-shield fa-fw mr-1"></i>Admin</a>
                                 </div>
                             </div>
