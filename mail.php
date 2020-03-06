1a9d23fa8defcf52fad3d8cb4f7fe46939b2415b
Carrère <ludovic.carrere@viacesi.fr>
2020-03-06 15:24:25 +0100

    Formulaire de contact

--- mail.php
+++ mail.php
@@ -9,26 +9,26 @@ use  PHPMailer \ PHPMailer \ PHPMailer ;
 
 $mail = new  PHPMailer();
 // $ mail-> SMTPDebug = SMTP :: DEBUG_SERVER;
-$mail -> IsSMTP ();                           // dire à la classe d'utiliser SMTP
+$mail->IsSMTP ();                           // dire à la classe d'utiliser SMTP
 // Définit le nom d'hôte du serveur de messagerie
-$mail -> Host = 'smtp.mailtrap.io' ;
+$mail->Host = 'smtp.mailtrap.io' ;
 // Définissez le numéro de port SMTP - susceptible d'être 25, 465 ou 587
-$mail -> Port = 465 ;
+$mail->Port = 465 ;
 
 // Utiliser ou non l'authentification SMTP
-$mail -> SMTPAuth = true ;
+$mail->SMTPAuth = true ;
 // Nom d'utilisateur à utiliser pour l'authentification SMTP
-$mail -> Username = '2551c2d91a8178' ;
+$mail->Username = '49cedd2967dc39' ;
 // Mot de passe à utiliser pour l'authentification SMTP
-$mail -> Password = '15dcdfab01d336' ;
+$mail->Password = '578969342ac67c' ;
 // Définit de qui le message doit être envoyé
-$mail -> setFrom ( 'H.isik1@outlook.fr' );
+$mail->setFrom ($_POST['email']);
 // Définissez la ligne d'objet
-$mail -> addAddress ( 'fanokiller54@gmail.com' );
-$mail -> Subject = 'PHPMailer Test' ;
+$mail->addAddress ( 'carrere.ludovic54@gmail.com' );
+$mail->Subject = $_POST['subject'] ;
 // Lire un corps de message HTML à partir d'un fichier externe, convertir des images référencées en images incorporées,
 // convertit HTML en un corps alternatif de base en texte brut
-$mail -> Body = 'C \' est un mail test ' ;
+$mail->Body = $_POST['msg'] ;
 
 if (! $mail -> send ()) {
     echo  'Mailer Error:' .$mail -> ErrorInfo ;
