d352f5d739582052becc3ddb143af5584fa1da49
Carrère <ludovic.carrere@viacesi.fr>
2020-03-06 15:25:46 +0100

    Gestion du mail de contact

--- formulaire/form.php
+++ formulaire/form.php
@@ -1,66 +1,37 @@
 <?php include __DIR__ . '/../header.php' ?>
 <div class="main-content flex-1 bg-gray-100 mt-12 md:mt-2 pb-24 md:pb-5">
-    <form class="w-full max-w-lg mx-auto mt-12" action="mailto:w.daoud@laposte.net">
+    <form class="w-full max-w-lg mx-auto mt-12" action="/../ProjetWeb/mail.php" enctype="multipart/form-data"method="POST">
         <div class="flex flex-wrap mb-6">
             <div class="w-full md:w-1/2 px-3">
-                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="prenom">
                     Prénom
                 </label>
-                <input class="appearance-none block w-full bg-gray-300 text-gray-700 border border-gray-300 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-first-name" type="text" placeholder="Jean">
+                <input class="appearance-none block w-full bg-gray-300 text-gray-700 border border-gray-300 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="prenom" type="text" placeholder="Jean" name="prenom">
             </div>
             <div class="w-full md:w-1/2 px-3">
-                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
+                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="nom">
                     Nom
                 </label>
-                <input class="appearance-none block w-full bg-gray-300 text-gray-700 border border-gray-300 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Dupont">
+                <input class="appearance-none block w-full bg-gray-300 text-gray-700 border border-gray-300 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="nom" type="text" placeholder="Dupont" name="nom">
             </div>
         </div>
         <div class="flex flex-wrap mb-2">
-            <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-city">
-                    Ville
-                </label>
-                <input class="appearance-none block w-full bg-gray-300 text-gray-700 border border-gray-300 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text" placeholder="Nancy">
-            </div>
-            <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-zip">
-                    Code postal
-                </label>
-                <input class="appearance-none block w-full bg-gray-300 text-gray-700 border border-gray-300 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-zip" type="text" placeholder="54000">
-            </div>
-            <div class="w-full px-3 mb-6 md:mb-0 p-6">
-                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
-                    Région
-                </label>
-                <div class="relative">
-                    <select class="block appearance-none w-full bg-gray-300 border border-gray-300 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
-                        <option>Grand-Est</option>
-                        <option>Hauts-de-France</option>
-                        <option>Normandie</option>
-                        <option>Bretagne</option>
-                        <option>Pays de la Loire</option>
-                        <option>Centre-Val de Loire</option>
-                        <option>Île de France</option>
-                        <option>Bourgogne-Franche-Comté</option>
-                        <option>Auverge-Rhône-Alpes</option>
-                        <option>Nouvelle Aquitaine</option>
-                        <option>Occitanie</option>
-                        <option>Provence-Alpes-Côtes-d'Azur</option>
-                        <option>Corse</option>
-                        <option>Autre</option>
-                    </select>
-                    <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                        <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                            <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
-                    </div>
-                </div>
-            </div>
+            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="email">
+                    Email
+            </label>
+            <input class="appearance-none block w-full bg-gray-300 text-gray-700 border border-gray-300 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="email" type="mail" placeholder="exemple@test.fr" name="email">
+        </div>
+        <div class="flex flex-wrap mb-2">
+            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="subject">
+                    Sujet
+            </label>
+            <input class="appearance-none block w-full bg-gray-300 text-gray-700 border border-gray-300 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="subject" type="text" name="subject">
         </div>
         <div class="w-full px-3 mb-6 md:mb-0">
-            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-question">
+            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="msg">
                 Votre question
             </label>
-            <textarea class="w-full bg-gray-300 focus:bg-white" placeholder="Décrivez votre question ici..."></textarea>
+            <textarea class="w-full bg-gray-300 focus:bg-white" name="msg" placeholder="Décrivez votre question ici..."></textarea>
         </div>
         <div class="px-5 text-center p-4">
             <button class="w-40 shadow bg-gray-700 hover:bg-gray-600 focus:shadow-outline focus:outline-none text-gray-300 font-bold py-2 px-4 rounded" type="submit"><i class="mr-1 fas fa-envelope-open-text"></i>
