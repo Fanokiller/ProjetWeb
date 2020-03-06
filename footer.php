80d30dbc5d738a277d27cc6c73c45a27d2a17495
Jean-Baptiste Brausem <j.b.brausem@gmail.com>
2020-03-06 14:10:43 +0100

    ajout bouton contactez-nous

--- footer.php
+++ footer.php
@@ -16,5 +16,12 @@
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+<div class="flex" href="../formulaire/form.php">
+    <a href="../formulaire/form.php" class="flex mx-auto w-1/3">
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded w-full mx-auto" href="../formulaire/form.php">Contactez-nous</button>
+    </a>
+
+</div>
 </body>
 </html>
